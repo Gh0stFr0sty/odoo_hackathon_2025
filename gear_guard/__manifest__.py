dd795ad58f372ae7e8be21703d50602143332c89
Gh0stFr0sty <csghost169@gmail.com>
2025-12-27 03:28:06 +0000

    Folder structure creation

---
+++ gear_guard/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'GearGuard - Maintenance Tracker',
+    'version': '1.0',
+    'summary': 'Asset and maintenance management module',
+    'description': 'Odoo Hackathon project - GearGuard',
+    'author': 'Odoo Hackathon Team',
+    'category': 'Operations',
+    'depends': ['base', 'mail'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'application': True,
+}
